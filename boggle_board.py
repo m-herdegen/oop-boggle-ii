b43853a1f11b9600310f9a9f9aa20b63e8e1bbc3
m-herdegen <93681016+m-herdegen@users.noreply.github.com>
2022-06-02 18:00:28 -0400

    still need diagonals

--- boggle_board.py
+++ boggle_board.py
@@ -1,11 +1,73 @@
 # You should re-use and modify your old BoggleBoard class to support the new requirements
+import random 
 
 class BoggleBoard:
-	def __init__(self):
-      pass
 
-	def shake(self):
-      pass
-  
-  def include_word(self):
-      pass
+      dice_list = []
+
+      for i in range(16):
+            die = []
+            for x in range(6):
+                  die.append((chr(random.randint(ord('A'), ord('Z')))))
+            dice_list.append(die)
+            random.shuffle(dice_list)
+
+      def __init__(self):
+            self.board = ''
+            for x in range(16):
+                  if x % 4 == 0:
+                        self.board += '\n'
+                  self.board += '_'
+            self.board += '\n'
+            print(*self.board)
+
+      def shake(self):
+            self.board = ''
+            new_letter = ''
+            self.board_list = []
+            for i, x in enumerate(BoggleBoard.dice_list):
+                  if i % 4 == 0:
+                        self.board += '\n'
+                  new_letter = x[random.randint(0, 5)]
+                  self.board_list.append(new_letter)
+                  if new_letter == 'Q':
+                        self.board += new_letter + 'u '
+                  else:
+                        self.board += new_letter + '  '
+            self.board += '\n'
+
+            print('{:}'.format(self.board))
+            #print(self.board_list)
+
+            return self.board_list
+
+      def include_word(self):
+            temp = []
+            horizontal_list = []
+            vertical_list = []
+            diagonal_list = []
+
+            for value in self.board_list:
+
+                  temp.append(value)
+                  if len(temp) == 4:
+                        horizontal_list.append(temp)
+                        temp = []
+
+            for i in range(4):
+                  for value in horizontal_list:
+                        temp.append(value[i])
+                  vertical_list.append(temp)
+                  temp = []
+                        
+            print(*vertical_list)
+                  
+
+            print(*horizontal_list)
+
+
+
+
+board_new = BoggleBoard()
+board_new.shake()
+board_new.include_word() # => True or False
